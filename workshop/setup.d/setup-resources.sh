f51992d0378967be01b608009bb6bf703a7affb5
samar singhal <samarsinghal@ssamar-a01.vmware.com>
2021-06-20 20:15:53 -0600

    updated scg lab

--- workshop/setup.d/setup-resources.sh
+++ workshop/setup.d/setup-resources.sh
@@ -4,4 +4,6 @@ envsubst < demo/my-gateway.yaml.in > demo/my-gateway.yaml
 envsubst < demo/my-gateway-ingress.yaml.in > demo/my-gateway-ingress.yaml
 envsubst < demo/helloworld-gateway-mapping.yaml.in > demo/helloworld-gateway-mapping.yaml
 envsubst < demo/github-gateway-mapping.yaml.in > demo/github-gateway-mapping.yaml
+envsubst < demo/github-route-config.yaml.in > demo/github-route-config.yaml
+envsubst < demo/github-ratelimit-route-config.yaml.in > demo/github-ratelimit-route-config.yaml
 
